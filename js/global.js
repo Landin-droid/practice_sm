da05b3e780577922d3b6d3d72323cde76e1894bf
Landon-droid <lyamkin.dimentor1@mail.ru>
2025-07-13 14:29:06 +0700

    decreased js file size

--- js/global.js
+++ js/global.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const mobileMenuOverlay = document.getElementById("mobile-menu-overlay");
   const mobileMenuPanel = document.getElementById("mobile-menu-panel");
 
-  // Открытие мобильного меню
   mobileMenuButton.addEventListener("click", function () {
     mobileMenu.classList.remove("hidden");
     setTimeout(() => {
@@ -15,22 +14,18 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.style.overflow = "hidden";
   });
 
-  // Закрытие мобильного меню
   function closeMenu() {
     mobileMenuOverlay.classList.remove("open");
     mobileMenuPanel.classList.remove("open");
     setTimeout(() => {
       mobileMenu.classList.add("hidden");
       document.body.style.overflow = "";
-    }, 300); // Должно совпадать с duration анимации
+    }, 300);
   }
 
   closeMobileMenuButton.addEventListener("click", closeMenu);
-
-  // Закрытие при клике на оверлей
   mobileMenuOverlay.addEventListener("click", closeMenu);
 
-  // Закрытие при клике на ссылку в меню
   const mobileMenuLinks = mobileMenuPanel.querySelectorAll("a");
   mobileMenuLinks.forEach((link) => {
     link.addEventListener("click", closeMenu);
@@ -39,17 +34,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
-// Показать/скрыть кнопку при прокрутке
 window.addEventListener("scroll", () => {
   if (window.scrollY > 150) {
-    // Показать, если прокручено более 150px
     scrollToTopBtn.classList.remove("hidden");
   } else {
     scrollToTopBtn.classList.add("hidden");
   }
 });
 
-// Плавная прокрутка наверх при клике
 scrollToTopBtn.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
